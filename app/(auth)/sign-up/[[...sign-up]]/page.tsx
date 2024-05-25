ea4b54ef17ada936e5b4281fd1296b375a231dea
JosueIsOffline <josuehernandez2314@gmail.com>
2024-05-24 23:22:30 -0400

    Authentication

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,34 @@
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react"
+import  Image  from "next/image";
+
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div
+        className="h-full lg:flex flex-col items-center
+       justify-center px-4"
+      >
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or Create account to get to your dashboard!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+             <SignUp path="/sign-up" />
+          </ClerkLoaded>
+         <ClerkLoading>
+            <Loader2 className="animete-spin text-muted-foreground" />
+         </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-black hidden lg:flex items-center
+      justify-center">
+        <Image src="/logo.svg" height={100} width={100} alt="logo"/>
+      </div>
+    </div>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+import { UserButton } from "@clerk/nextjs"
+
+export default function Home() {
+  return (
+    <UserButton afterSignOutUrl="/"/>
+  );
+}
