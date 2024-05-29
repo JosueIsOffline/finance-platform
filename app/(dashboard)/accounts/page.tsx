5f568bf022466e38fe9e6de38e79fc88782e8a7a
JosueIsOffline <josuehernandez2314@gmail.com>
2024-05-28 23:33:07 -0400

    Data table completed

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,58 +1,53 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle
-
-} from "@/components/ui/card"
-import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts"
-import { Plus } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+import { Plus } from "lucide-react";
 
 import { columns, Payment } from "./columns";
 import { DataTable } from "@/components/data-table";
 
-
-
-  const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-        id: "728ed52f",
-        amount: 5,
-        status: "success",
-        email: "a@example.com",
-      },
-  ]
-
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 5,
+    status: "success",
+    email: "a@example.com",
+  },
+];
 
 const AccountsPage = () => {
-    const newAccount = useNewAccount()
+  const newAccount = useNewAccount();
 
-    return (
-        <div className="max-x-screen-2xl mx-auto w-full pb-10 -mt-24">
-           <Card className="border-none drop-shadow-sm">
-                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-                    <CardTitle className="text-xl line-clamp-1">
-                        Account page
-                    </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
-                        <Plus className="size-4 mr-2"/>
-                        Add new
-                    </Button>
-                </CardHeader>
-                <CardContent>
-                    <DataTable columns={columns} data={data} filterKey={}/>
-                </CardContent>
-           </Card>
-        </div>
-    )
-}
+  return (
+    <div className="max-x-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Account page</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={data}
+            filterKey="email"
+            onDelete={() => {}}
+            
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default AccountsPage
+export default AccountsPage;
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -3,6 +3,7 @@
 import {
   ColumnDef,
   ColumnFiltersState,
+  Row
   flexRender,
   getCoreRowModel,
   useReactTable,
@@ -23,22 +24,29 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import React from "react";
+import { Trash } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
-  data: TData[]
-  filterKey: string
+  data: TData[];
+  filterKey: string;
+  onDelete: (rows: Row<TData>[]) => void;
+  disabled?: boolean;
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
-  filterKey
+  filterKey,
+  onDelete,
+  disabled
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
+  const [rowSelection, setRowSelection] = React.useState({});
+
   const table = useReactTable({
     data,
     columns,
@@ -46,11 +54,13 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
-    onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    onRowSelectionChange: setRowSelection,
     state: {
       sorting,
       columnFilters,
+      rowSelection,
     },
   });
 
@@ -65,6 +75,17 @@ export function DataTable<TData, TValue>({
           }
           className="max-w-sm"
         />
+        {table.getFilteredSelectedRowModel().rows.length > 0 && (
+          <Button
+            disabled={disabled}
+            size="sm"
+            variant="outline"
+            className="ml-auto font-normal text-xs"
+          >
+            <Trash className="size-4 mr-2" />
+              Delete ({ table .getFilteredSelectedRowModel().rows.length })
+          </Button>
+        )}
       </div>
 
       <div className="rounded-md border">
@@ -118,6 +139,10 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
         <Button
           variant="outline"
           size="sm"
